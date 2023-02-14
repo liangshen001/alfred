abacf5375c9cf20cc856ecb22943a882f2151259
liangshen001 <wangliang520wl@hotmail.com>
2023-02-14 20:07:48 +0800

    perf: 使用rollup构建 添加自动部署工作流

--- bin/init.js
+++ bin/init.js
@@ -9,6 +9,10 @@ let projectPath = path.resolve(process.cwd(), '../../../');
 let infoPath = path.resolve(projectPath, 'info.plist');
 if (!fs.existsSync(infoPath)) {
     let info = fs.readFileSync(path.resolve(process.cwd(), '', 'template/info.plist'), 'UTF-8').toString();
+    let packageJsonPath = path.resolve(projectPath, 'package.json');
+    if (!fs.existsSync(packageJsonPath)) {
+        return;
+    }
     const data = fs.readFileSync(path.resolve(projectPath, 'package.json'), 'UTF-8').toString();
     if (data) {
         let config = JSON.parse(data);
